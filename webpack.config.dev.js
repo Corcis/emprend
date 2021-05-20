3ec034608e9a64fcc4120baff529fadd601c581b
Diana Cortés <corcisdr@gmail.com>
2021-05-20 10:11:37 -0500

    Regresando mi puerto de servicio a 3001

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -65,7 +65,7 @@ module.exports = {
         contentBase: path.join(__dirname, 'dist'),
         historyApiFallback: true,
         compress: true,
-        port: 3002
+        port: 3001
     },
     plugins: [
         new HtmlWebpackPlugin({
